26d30d24fbe4dba7cee01919ca94f57060c3fbc2
Kark Leo <v.malykhin.ff@gmail.com>
2018-10-10 18:06:19 +0300

    March 2018

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function scrollbarWidth() {
+    var block = $('<div>').css({'height':'50px','width':'50px'}),
+        indicator = $('<div>').css({'height':'200px'});
+
+    $('body').append(block.append(indicator));
+    var w1 = $('div', block).innerWidth();    
+    block.css('overflow-y', 'scroll');
+    var w2 = $('div', block).innerWidth();
+    $(block).remove();
+    return (w1 - w2);
+}
+
+document.documentElement.style.setProperty('--ws', scrollbarWidth()+'px'); 
+
